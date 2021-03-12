d4552f319e99f6ff11f482f3b62de31582f9975f
Chris Caron Sunil <caron2001@gmail.com>
2021-03-12 15:35:42 +0530

    Uploads picture to firebase

--- expense_manager/lib/Profile.dart
+++ expense_manager/lib/Profile.dart
@@ -4,19 +4,26 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-import 'charts/Chart_Month/WeekinMonth_class.dart';
-import 'charts/Chart_Month/LineCharts.dart';
-import 'charts/Chart_Month/WeekinMonth_chart.dart';
-import 'charts/Chart_Week/DaysinWeek_chart.dart';
-import 'charts/Chart_Week/DaysinWeek_class.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:expense_manager/charts/Lent/lent_class.dart';
 import 'package:expense_manager/charts/Lent/lent_chart.dart';
 import 'package:expense_manager/charts/Owe/owe_class.dart';
 import 'package:expense_manager/charts/Owe/owe_chart.dart';
 import 'edit_profile.dart';
+import 'firestorageservice.dart';
 
+String userAvatarUrl='https://www.veterinarypracticenews.com/wp-content/uploads/2019/08/bigstock-Little-Striped-Cute-Kitten-Sit-244080397.jpg';
+
+/*
+* Profile picture can be uploaded in the edit profile page
+* It is stored in firebase storage in image/img.uid
+* Failed trying to display the picture on screen
+* Something about async functions I think, Its only displaying the default picture and not waiting for the
+* async method to check to see if there is a profile picture in firebase storage
+* We know it does check to see because I print out the image and it gives me a link to the image
+* The state doesnt set with displaying the picture stored in firebase
+*
+* */
 
 class Profile extends StatefulWidget {
   final User curr;
@@ -35,17 +42,68 @@ class _ProfileState extends State<Profile> {
   final List<Oweobject> owelist= List<Oweobject>();
   FirebaseAuth auth = FirebaseAuth.instance;
   User curr;
+  String uid;
   String username;
+  Image profileImage;
+  Image profileImage1;
+  //bool finish=false;
+
 
   @override
-  void initState() {
+  void initState(){
+    imageFromStorage(context, 'image/img.$uid')?.then((img){
+      setState(() {
+        profileImage=img;                                             /*This isnt working. idk.*/
+      });
+    });
+    if(profileImage==null){
+      print('Its null in init');
+      profileImage=Image.asset('images/defaultProfile.png');
+    }
     super.initState();
+  }
+
+  Future imageFromStorage(BuildContext context, String filePath) async{
+    Image m;
+    String image=filePath;
+    await FireStorageService?.loadFromStorage(context, image)?.then((downloadUrl) { //A function we defined that
+      m = Image.network(                                                    //returns the URL of the picture
+        downloadUrl.toString(),
+        fit: BoxFit.scaleDown,
+      );
+    });
+    return m;                         //returns an Image
+  }
+
+  Future displayPicture() async {
+    profileImage1 = await this?.imageFromStorage(
+        context, 'image/img.$uid'); // Calls the function to go to storage
+                                    //And acquire the picture
+    if(profileImage1==null){
+      print('Its null in display');
+      profileImage1=Image.asset('images/defaultProfile.png');
+    }
+
+    setState(() {                     //This whole setstate causes the page to load super slow and it apparantly keeps getting
+      profileImage=profileImage1;     //called infinitely. so. Take out this setstate, change the ^ profileimage1's to profileimage
+      print('in profile page');
+      print(profileImage);
+    });
 
   }
+
+
 
   Widget build(BuildContext context) {
     curr= auth.currentUser;
     username=curr.displayName;
+
+    setState(() {
+      displayPicture();
+      print('now the image is:');
+      print(profileImage);
+    });
+
   
     return Scaffold(
       body:Column(
@@ -98,11 +156,10 @@ class _ProfileState extends State<Profile> {
                          children: [
                            Row(
                              children: [
-                               Image.asset(
-                                 'images/defaultProfile.png',
-                                 colorBlendMode: BlendMode.colorDodge,
-                                 width: 75,
-                                 height: 75,
+                               CircleAvatar(
+
+                                 backgroundImage: profileImage.image,//!=null?profileImage.image:Image.asset('images/defaultProfile.png').image,//NetworkImage(userAvatarUrl),
+                                 radius: 35,
                                ),
                                SizedBox(
                                  width: 10,
@@ -270,3 +327,26 @@ class _ProfileState extends State<Profile> {
 
 
 
+
+// Future UpdatePic() async{
+//   String fileName = basename(_image.path);
+//   StorageReference firebaseStorageRef =
+//   FirebaseStorage.instance.ref().child(fileName);
+//   StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
+//   StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+//   print("Picture updated successfully.");
+// }
+// import 'charts/Chart_Month/WeekinMonth_class.dart';
+// import 'charts/Chart_Month/LineCharts.dart';
+// import 'charts/Chart_Month/WeekinMonth_chart.dart';
+// import 'charts/Chart_Week/DaysinWeek_chart.dart';
+// import 'charts/Chart_Week/DaysinWeek_class.dart';
+
+
+
+// if(finish) {
+//   setState(() {
+//     profileImage = profileImage;
+//     finish=false;
+//   });
+// }
--- expense_manager/lib/edit_profile.dart
+++ expense_manager/lib/edit_profile.dart
@@ -3,6 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:expense_manager/Profile.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'firestorageservice.dart';
+
+String userAvatarUrl='https://www.veterinarypracticenews.com/wp-content/uploads/2019/08/bigstock-Little-Striped-Cute-Kitten-Sit-244080397.jpg';
+
 
 class EditProfile extends StatefulWidget {
   @override
@@ -14,11 +21,88 @@ class _EditProfileState extends State<EditProfile> {
   FirebaseAuth auth = FirebaseAuth.instance;
   TextEditingController _controller;
   User curr;
+  String uid;
   String username;
+  File _image;
+  Image profileImage;
+  final picker = ImagePicker();
+
+
+  @override
+  void initState(){
+
+    //displayPicture();
+    this?.ImageFromStorage(context, 'image/img.$uid')?.then((img){
+      setState(() {
+        profileImage=img;
+      });
+    });
+    if(profileImage==null){
+      print('Its null in init');
+      profileImage=Image.asset('images/defaultProfile.png');
+    }
+    super.initState();
+  }
+
+  Future getImage() async { //Gets an image
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+        print(_image);
+        print('picture selected');
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future ImageFromStorage(BuildContext context, String filePath) async{ //Retrieves the image from where its stored
+    Image m;
+    String image=filePath;
+    await FireStorageService?.loadFromStorage(context, image)?.then((downloadUrl) {
+      m = Image.network(
+        downloadUrl.toString(),
+        fit: BoxFit.scaleDown,
+      );
+    });
+
+    return m;
+
+  }
+
+  Future uploadPic() async {
+    if (_image != null) {
+      StorageReference ref = FirebaseStorage.instance.ref();
+      StorageTaskSnapshot addImg = await ref
+          .child("image/img.$uid")
+          .putFile(_image)
+          .onComplete;
+      if (addImg.error == null) {
+        print("added to Firebase Storage");
+      }
+    }
+    setState(() {
+      displayPicture();     // Calls the function to retrieve the image
+      //soon afetr we store it in firebase storage
+    });
+
+
+  }
+
+
+  Future displayPicture() async {
+    profileImage = await this?.ImageFromStorage(
+        context, 'image/img.$uid'); // Calls the function to go to storage
+    //And acquire the picture
+    print(profileImage);
+    print('in edit profile');
+  }
 
   @override
   Widget build(BuildContext context) {
     curr= auth.currentUser;
+    uid=curr.uid;
     username=curr.displayName;
     _controller = new TextEditingController(text: '$username');
     return ModalProgressHUD(
@@ -37,10 +121,14 @@ class _EditProfileState extends State<EditProfile> {
               ),
               FlatButton(
                 onPressed: ()async{
+
                   if(username!=null&& username!='') {
                     setState(() {
                       showSpinner=true;
                     });
+
+                  uploadPic();//-> put it in the getImage function?
+
                     try {
                       await curr.updateProfile(displayName: username);
                       Navigator.push(
@@ -82,6 +170,7 @@ class _EditProfileState extends State<EditProfile> {
               ),
 
             ],
+
           ),
 
         ),
@@ -90,14 +179,14 @@ class _EditProfileState extends State<EditProfile> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Image.asset(
-                  'images/defaultProfile.png',
-                  colorBlendMode: BlendMode.colorDodge,
-                  width: 75,
-                  height: 75,
+                CircleAvatar(
+                  backgroundImage: profileImage!=null?profileImage.image:Image.asset('images/defaultProfile.png').image,//NetworkImage(userAvatarUrl),
+                  radius: 45,
                 ),
                 FlatButton(
-                  onPressed: (){},
+                   onPressed:() async {
+                     getImage();
+                     },
                   child: Text(
                       'Edit Profile Picture'
                   ),
@@ -143,87 +232,11 @@ class _EditProfileState extends State<EditProfile> {
 
   }
 }
-
-// class EditProfile extends StatelessWidget {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//
-//
-//     // return Scaffold(
-//     //   appBar: AppBar(
-//     //     title: Row(
-//     //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//     //       children: [
-//     //         Text(
-//     //           'Edit Profile',
-//     //           style: TextStyle(
-//     //             color: Colors.white,
-//     //             fontWeight: FontWeight.bold,
-//     //           ),
-//     //         ),
-//     //         FlatButton(
-//     //             onPressed: (){
-//     //               Navigator.pop(context);
-//     //             },
-//     //             child: Text(
-//     //               'SAVE',
-//     //               style: TextStyle(
-//     //                 color: Colors.white,
-//     //               ),
-//     //             ),
-//     //           color: Colors.blue,
-//     //         ),
-//     //
-//     //       ],
-//     //     ),
-//     //
-//     //   ),
-//     //   body: Padding(
-//     //     padding: const EdgeInsets.all(8.0),
-//     //     child: Column(
-//     //       crossAxisAlignment: CrossAxisAlignment.stretch,
-//     //       children: [
-//     //
-//     //         Image.asset(
-//     //           'images/defaultProfile.png',
-//     //           colorBlendMode: BlendMode.colorDodge,
-//     //           width: 75,
-//     //           height: 75,
-//     //         ),
-//     //         FlatButton(
-//     //             onPressed: (){},
-//     //             child: Text(
-//     //               'Edit Profile Picture'
-//     //             ),
-//     //           //color: Colors.white,
-//     //         ),
-//     //         SizedBox(
-//     //           height: 10,
-//     //         ),
-//     //         Row(
-//     //           mainAxisAlignment: MainAxisAlignment.start,
-//     //           children: [
-//     //             Text(
-//     //               'Name',
-//     //               style: TextStyle(
-//     //                 fontWeight: FontWeight.bold,
-//     //               ),
-//     //             ),
-//     //             // TextField(
-//     //             //   controller: _controller,
-//     //             //   onSubmitted: (String value)async{
-//     //             //     await curr.updateProfile(displayName: value);
-//     //             //   },
-//     //             // ),
-//     //
-//     //
-//     //
-//     //               ],
-//     //         )
-//     //       ],
-//     //     ),
-//     //   ),
-//     // );
-//   }
-// }
+// String fileName = basename(_image.path);
+// StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child(fileName);
+// StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
+// StorageTaskSnapshot taskSnapshot=await uploadTask.onComplete;
+// setState(() {
+//   print("Profile Picture uploaded");
+//   Scaffold.of(context).showSnackBar(SnackBar(content: Text('Profile Picture Uploaded')));
+// });
